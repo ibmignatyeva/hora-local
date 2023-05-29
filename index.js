eb41c25ad28d5461a1d44e41bf54f96a10f8de97
Ive Ignatyeva <109037599+ibmignatyeva@users.noreply.github.com>
2023-05-29 15:18:32 -0300

    fixed problem with img src path. fixed css and html

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function loadingPicture() {
   var img = document.getElementById("image");
   var date = new Date();
 
+
   let hour = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
@@ -12,17 +13,17 @@ function loadingPicture() {
 
   if (hour >= 0 && hour < 12) {
     msg.innerHTML += `<h3>Good morning!<h3> `;
-    img.src = 'morning.jpg'
+    img.src = "./img/morning.jpg";
     document.body.style.background = `#FEE069`;
 
   } else if (hour >= 12 && hour < 18) {
     msg.innerHTML += `<h3>Good afternoon!<h3>`;
-      img.src = 'afternoon.jpg';
+      img.src = "./img/afternoon.jpg";
     document.body.style.background = `#b9846f`;
 
   } else {
     msg.innerHTML += `<h3>Good evening!<h3> `;
-      img.src = 'evening.jpg';
+      img.src = "./img/nigth.jpg";
    document.body.style.background = `#07284b`;
   }
 }
